e144d2af33eb1c053f5771a228fadad8419cf4b7
Ulf R. Pedersen <urp@nsm-i165>
2017-10-30 20:42:43 +0100

    asdasdl,x,cl,v

--- js/Element.js
+++ js/Element.js
@@ -1,23 +1,48 @@
 class Element {
 
-  constructor(type,id,x,y) {
-    this.type = type;
+  constructor(id,type,capacity,level,x,y) {
     this.id = id;
+    this.type = type;
+    this.capacity = capacity;
+    this.level = level  
     this.x = x;
     this.y = y;
     this.connections = [];
     console.log("Construct " + this.type + " at (" + x + ", " + y + "), index = " + this.id);
-
-    //swich(this.type)
+    console.log("  level = " + this.level + ",  capacity = " + this.capacity);
   }
 
-  /*setO(O){
-     this.O = =;
-  }*/
-
-  connect(e){
-    this.connections.push(e);
-    e.connections.push(this);
+  setLevel(level){
+     this.level = level;
   }
 
+  setCapacity(capacity){
+     this.capacity = capacity;
+  }
+
+  connect(element){
+    this.connections.push(element);
+    element.connections.push(this);
+  }
+
+  update(dt){
+    switch(this.type){
+      case "o-storage":
+      // ARGhhhh, det er svært ...
+      break;
+    }
+  }
+
+/*
+  update(dt) {
+    swich(this.type){
+    case "leak":
+      //   
+      break;
+    case "o-storage":
+      //   
+      break;
+    }
+  }
+*/
 }
--- js/main.js
+++ js/main.js
@@ -3,9 +3,9 @@ var elements = [];
 window.onload = function() {
   console.log("load setup()");
 
-  var first = new Element("o-storage",0,10,10);
+  var first = new Element(0,"o-storage",1.0,1.0,10,10);
   elements.push(first);
-  var second = new Element("leak",1,30,60);
+  var second = new Element(1,"leak",1.0,1.0,30,60);
   first.connect(second);
   elements.push(second);
 
